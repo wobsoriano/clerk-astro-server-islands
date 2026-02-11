074c033efdbe924755d760bf0971864f05a6df10
wobsoriano <sorianorobertc@gmail.com>
2026-02-11 14:37:08 -0800

    switch to vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,15 @@
 import { defineConfig } from 'astro/config'
-import node from '@astrojs/node'
+// import node from '@astrojs/node'
 import clerk from '@clerk/astro'
 
 import tailwindcss from '@tailwindcss/vite';
 
+import vercel from '@astrojs/vercel';
+
 export default defineConfig({
   integrations: [clerk()],
-  adapter: node({ mode: 'standalone' }),
+  adapter: vercel(),
   output: 'server',
-
   vite: {
     plugins: [tailwindcss()],
   },
